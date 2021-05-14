404bec713d8dde8b0574b83ba6eed960a484ba92
chosoonseob <eyedial@naver.com>
2021-05-14 14:06:07 +0900

    update 완료

--- MyProjectX/app/src/main/java/com/example/myprojectx/MainActivity.java
+++ MyProjectX/app/src/main/java/com/example/myprojectx/MainActivity.java
@@ -8,9 +8,16 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.myprojectx.ATask.LoginSelect;
+
+import java.util.concurrent.ExecutionException;
+
+import static com.example.myprojectx.Common.CommonMethod.loginDTO;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +35,49 @@ public class MainActivity extends AppCompatActivity {
         etPw = findViewById(R.id.etPASSWD);
 
         // 로그인 버튼
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etId.getText().toString().length() != 0 && etPw.getText().toString().length() != 0){
+                    String id = etId.getText().toString();
+                    String passwd = etPw.getText().toString();
 
+                    LoginSelect loginSelect = new LoginSelect(id, passwd);
+                    try {
+                        loginSelect.execute().get();
+                    } catch (ExecutionException e) {
+                        e.getMessage();
+                    } catch (InterruptedException e) {
+                        e.getMessage();
+                    }
+
+                } else {
+                    Toast.makeText(MainActivity.this, "아이디와 암호를 모두 입력하세요", Toast.LENGTH_SHORT).show();
+                    Log.d("main:login", "아이디와 암호를 모두 입력하세요 !!!");
+                    return;
+                }
+
+                if(loginDTO != null){
+                    Toast.makeText(MainActivity.this, "로그인 되었습니다 !!!", Toast.LENGTH_SHORT).show();
+                    Log.d("main:login", loginDTO.getId() + "님 로그인 되었습니다 !!!");
+
+                    // 로그인 정보에 값이 있으면 로그인이 되었으므로 메인화면으로 이동
+                    if(loginDTO != null){
+                        Intent intent = new Intent(getApplicationContext(), Sub1Activity.class);
+                        startActivity(intent);
+
+                        //finish();
+                    }
+
+                }else {
+                    Toast.makeText(MainActivity.this, "아이디나 비밀번호가 일치안함 !!!", Toast.LENGTH_SHORT).show();
+                    Log.d("main:login", "아이디나 비밀번호가 일치안함 !!!");
+                    etId.setText(""); etPw.setText("");
+                    etId.requestFocus();
+                }
+
+            }
+        });
 
         // 회원가입버튼
         findViewById(R.id.btnJoin).setOnClickListener(new View.OnClickListener() {
@@ -46,7 +95,10 @@ public class MainActivity extends AppCompatActivity {
         String[] permissions = {
                 Manifest.permission.INTERNET,
                 Manifest.permission.ACCESS_NETWORK_STATE,
-                Manifest.permission.ACCESS_WIFI_STATE
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
         };
 
         int permissionCheck = PackageManager.PERMISSION_GRANTED;
